d29a53a6f1a6e5d7acb59dfcf8b7c1f49d34eedd
Andrew M <103536458+Ando-Mo@users.noreply.github.com>
2023-11-09 17:29:26 -0800

    doors correctly switch scenes

--- scenes/objects/gate.gd
+++ scenes/objects/gate.gd
@@ -3,4 +3,6 @@ extends StaticBody2D
 signal player_entered_gate
 
 func _on_area_2d_body_entered(_body):
-	player_entered_gate.emit()
+	var tween = create_tween()
+	tween.tween_property($"../Player","speed",0,0.5) 
+	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
