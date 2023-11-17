a03cf5963e5b9df783e4a9f20ef3d17485be7ba1
Andrew Mangal <amangal@ucsc.edu>
2023-11-16 23:55:31 -0800

    scene transitions

--- scenes/objects/gate.gd
+++ scenes/objects/gate.gd
@@ -5,4 +5,4 @@ signal player_entered_gate
 func _on_area_2d_body_entered(_body):
 	var tween = create_tween()
 	tween.tween_property($"../Player","speed",0,0.5) 
-	get_tree().change_scene_to_file("res://scenes/levels/inside.tscn")
+	TransitionLayer.change_scene("res://scenes/levels/inside.tscn")
